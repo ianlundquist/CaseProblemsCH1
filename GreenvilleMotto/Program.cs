64cb28b213a28af334e27c6b225538593d9cc297
Ian Lundquist <oe1550uh@go.minnstate.edu>
2020-01-19 19:34:18 -0600

    Added comment on top of program

--- GreenvilleMotto/Program.cs
+++ GreenvilleMotto/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ Authors: Kelley Fischer, Ian Lundquist
+ Date: 01/19/2020
+ Program description: Displays the motto for Greenville, Chapter 1 - Case Problem 1
+ Encountered issues: Just getting to know Git for Visual Studio
+ */
+using System;
 
 namespace GreenvilleMotto
 {
