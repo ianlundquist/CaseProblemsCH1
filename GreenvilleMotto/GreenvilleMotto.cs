53e34970649017b06ab42949dac6839a3dff3c6a
arkais <krfisc@outlook.com>
2020-01-19 19:48:15 -0600

    Changed class and filename, changed namespace to CaseProblems, also added comments

---
+++ GreenvilleMotto/GreenvilleMotto.cs
@@ -0,0 +1,19 @@
+﻿// Filename: GreenvilleMotto.cs
+// Written by: Ian Lundquist & Kelley Fischer
+// Written on: 01/19/20
+// Program Description: Displays the Greenville Motto
+// Encountered Problems: none
+
+
+using System;
+
+namespace CaseProblems
+{
+    class GreenvilleMotto
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The stars shine in Greenville.");
+        }
+    }
+}
